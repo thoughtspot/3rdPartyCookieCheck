a84fdfecd58f67957a4f77a7616de0e9a6ba1b61
Ashish Shubham <ashish.shubham@thoughtspot.com>
2025-09-16 15:34:23 -0700

    Add Lib

---
+++ lib/index.ts
@@ -0,0 +1,16 @@
+import { sendMessage } from "promise-postmessage";
+
+export const isThirdPartyCookiesEnabled = (): Promise<boolean> => {
+    const { resolve, promise } = Promise.withResolvers<boolean>();
+    const iframe = document.createElement("iframe");
+    iframe.src = "https://cookie-check.thoughtspot.app";
+    iframe.width = "1";
+    iframe.height = "1";
+    iframe.onload = async () => {
+        const enabled = await sendMessage(iframe, { type: "is-third-party-cookies-enabled" });
+        document.body.removeChild(iframe);
+        resolve(enabled);
+    };
+    document.body.appendChild(iframe);
+    return promise;
+};
