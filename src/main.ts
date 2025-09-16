49d1f2e74f321e17eada8ac86a05c4a649fc97f2
Ashish Shubham <ashish.shubham@thoughtspot.com>
2025-09-16 12:09:29 -0700

    Add 3rd party cookie check

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { onMessage } from "promise-postmessage";
+
+onMessage((message) => {
+  if (message.type !== "is-third-party-cookies-enabled") {
+    return;
+  }
+
+  // Try setting a cookie
+  document.cookie = "test3pc=1; SameSite=None; Secure";
+  const enabled = document.cookie.indexOf("test3pc=") !== -1;
+
+  return enabled;
+});
